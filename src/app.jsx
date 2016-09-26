e7d048978723fc326aaa1b98783956d64e777a25
Andrey Sokolov <sokolik_85@list.ru>
2016-09-26 14:33:35 +0700

    initial commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfira');
+var Firebase = require('firebase');
 
-var Hello = React.createClass({
+
+var App = React.createClass({
   render: function() {
     return <h1 className="red">
-      Hello!
+      App React!
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 ReactDOM.render(element, document.querySelector('.container'));
